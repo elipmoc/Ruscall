6e4cf7bb83a5e9db8a1341ddbab378b7c82dd1be
elipmoc <relipmoc101@gmail.com>
2018-08-11 12:36:13 +0900

    testコード修正

--- tests/test.rs
+++ tests/test.rs
@@ -34,29 +34,45 @@ fn parse_cmd_args_test() {
     );
 }
 
+use ruscall::compile::ast;
+//helper
+fn create_infixl_ast(op: &str, priority: i8) -> ast::StmtAST {
+    ast::StmtAST::InfixAST(ast::InfixAST {
+        op: op.to_string(),
+        ty: ast::InfixType::Left,
+        priority: priority,
+    })
+}
+fn create_infixr_ast(op: &str, priority: i8) -> ast::StmtAST {
+    ast::StmtAST::InfixAST(ast::InfixAST {
+        op: op.to_string(),
+        ty: ast::InfixType::Right,
+        priority: priority,
+    })
+}
+
 #[test]
 fn infix_parse_test() {
-    use ruscall::compile::ast;
     use ruscall::compile::parser::parse;
     assert_eq!(
         parse("infixl 4 +").unwrap().0,
         ast::ProgramAST {
-            stmt_list: vec![ast::StmtAST::create_infixl_ast("+".to_string(), 4)],
+            stmt_list: vec![create_infixl_ast("+", 4)],
         }
     );
     assert_eq!(
         parse("infixr 7 /").unwrap().0,
         ast::ProgramAST {
-            stmt_list: vec![ast::StmtAST::create_infixr_ast("/".to_string(), 7)],
+            stmt_list: vec![create_infixr_ast("/", 7)],
         }
     );
     assert_eq!(
         parse("  infixr 7 / \n infixl 9 * \tinfixr 1 -").unwrap().0,
         ast::ProgramAST {
             stmt_list: vec![
-                ast::StmtAST::create_infixr_ast("/".to_string(), 7),
-                ast::StmtAST::create_infixl_ast("*".to_string(), 9),
-                ast::StmtAST::create_infixr_ast("-".to_string(), 1),
+                create_infixr_ast("/", 7),
+                create_infixl_ast("*", 9),
+                create_infixr_ast("-", 1),
             ],
         }
     );
