1715470e6d1674c476e5c38b6341e96a2000ccc5
elipmoc <relipmoc101@gmail.com>
2018-10-31 17:09:47 +0900

    型制約実装とラムダ式の型修正

--- src/compile/types.rs
+++ src/compile/types.rs
@@ -1,21 +1,39 @@
+// 型変数のインデックス
+#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+pub struct TypeId(usize);
+
+impl TypeId {
+    pub fn new(id: usize) -> TypeId {
+        TypeId(id)
+    }
+    pub fn get_id(&self) -> usize {
+        self.0
+    }
+}
+
+//型制約
+#[derive(Clone, PartialEq)]
+pub enum TypeCondition {
+    Call(FuncType)
+}
+
 #[derive(Clone, PartialEq)]
 pub enum Type {
     Int32,
-    FuncType(Box<FuncType>),
     TupleType(Box<TupleType>),
-    TyVar(usize),
-    LambdaType(Box<LambdaType>)
+    TyVar(TypeId, Vec<TypeCondition>),
+    LambdaType(Box<LambdaType>),
 }
 
 impl Type {
-    pub fn create_func_type(param_types: Vec<Type>, ret_type: Type) -> Type {
-        Type::FuncType(Box::new(FuncType { param_types, ret_type }))
+    pub fn create_func_type(param_types: Vec<Type>, ret_type: Type, ty_id: TypeId) -> Type {
+        Type::LambdaType(Box::new(LambdaType { env_ty: None, func_ty: FuncType { param_types, ret_type } }))
     }
     pub fn create_tuple_type(element_tys: Vec<Type>) -> Type {
         Type::TupleType(Box::new(TupleType { element_tys }))
     }
-    pub fn create_lambda_type(env_tys:Vec<Type>,func_ty:FuncType)->Type{
-        Type::LambdaType(Box::new(LambdaType{env_tys,func_ty}))
+    pub fn create_lambda_type(env_tys: Vec<Type>, func_ty: FuncType) -> Type {
+        Type::LambdaType(Box::new(LambdaType { env_ty: Some(TupleType { element_tys: env_tys }), func_ty }))
     }
 }
 
@@ -33,6 +51,6 @@ pub struct TupleType {
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct LambdaType {
-    pub env_tys: Vec<Type>,
-    pub func_ty:FuncType
+    pub env_ty: Option<TupleType>,
+    pub func_ty: FuncType,
 }
