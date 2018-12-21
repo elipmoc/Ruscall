f003ecaf3ffa4d60cf63586b3bd03774e9ba4a19
elipmoc <relipmoc101@gmail.com>
2018-12-21 23:08:38 +0900

    自作のllvmラッパーを削除

--- src/my_llvm/value_ref.rs
+++
@@ -1,7 +0,0 @@
-extern crate llvm_sys as llvm;
-use self::llvm::prelude::LLVMValueRef;
-use super::helper::*;
-
-pub fn set_value_name(val: LLVMValueRef, name: &str) {
-    unsafe { llvm::core::LLVMSetValueName(val, string_cast(name).as_ptr()) }
-}
